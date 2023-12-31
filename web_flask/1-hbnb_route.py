da0fff107bb254153fbdea0bf451d48d8dfe2a52
Asega-bryan07 <Almasibryan7@gmail.com>
2023-12-31 20:15:05 +0300

    Web Framework

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,23 +2,24 @@
 """
 a script that starts a Flask web application listening on 0.0.0.0,
 port 5000
-    Routes:
-	/: display “Hello HBNB!”
+    Routes: display “Hello HBNB!”
 """
 
 from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     '''Returns a given string'''
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     '''Returns a given string '''
-    return ("HBNB!")
+    return "HBNB!"
 
 
 if __name__ == "__main__":
