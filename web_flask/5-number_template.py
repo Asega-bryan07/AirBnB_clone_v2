f73ac6519c03a2a434a6b496e39804aba0510812
Asega-bryan07 <Almasibryan7@gmail.com>
2024-01-01 13:01:20 +0300

    Web Framework

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,15 +39,15 @@ def pythonText(text="is cool"):
 @app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
     '''display “n is a number” only if n is an integer'''
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+    #if isinstance(n, int):
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n=None):
     '''display a HTML page only if n is an integer'''
-    if isinstance(n, int):
-        return render_template("5-number.html", n=n)
+    #if isinstance(n, int):
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
